352637e3ea8bd6c5ded4867e2fc7865c56242d66
artsantana <artsantana1457@hotmail.com>
2020-02-17 14:11:11 -0300

    Removed useless code

--- src/client/common/paletteColor.js
+++ src/client/common/paletteColor.js
@@ -2,21 +2,16 @@ let ColorChange = 'red';
 
 $("#redColor").click(() => {
   ColorChange = 'red';
-  console.log(ColorChange);
 })
 
 $("#blue").click(() => {
   ColorChange = 'blue';
-  console.log(ColorChange);
-
 })
 
 $("#white").click(() => {
   ColorChange = 'white';
-  console.log(ColorChange);
 })
 
 $("#yellow").click(() => {
   ColorChange = 'yellow';
-  console.log(ColorChange);
 })
