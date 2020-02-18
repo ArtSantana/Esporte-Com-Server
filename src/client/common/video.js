4361ea466f6ee5af826cb910fd6b7c75555ae719
artsantana <artsantana1457@hotmail.com>
2020-02-18 15:47:20 -0300

    Added the video selector to the menuVideo

--- src/client/common/video.js
+++ src/client/common/video.js
@@ -6,6 +6,7 @@ $("#btnVideo").click(() => {
     $("#bnt").css('visibility', 'hidden');
     $("#paletteColor").css('visibility', 'hidden');
     $("#menuVideo").css('visibility', 'visible');
+    $("#videoSelect").css('visibility', 'visible');
 })
 
 $("#backButton").click(() => {
@@ -13,8 +14,15 @@ $("#backButton").click(() => {
   $("#bnt").css('visibility', 'visible');
   $("#TimeA").css('visibility', 'visible');
   $("#menuVideo").css('visibility', 'hidden');
+  $("#videoSelect").css('visibility', 'hidden');
   $("#video").css('visibility', 'hidden');
 })
 
+$("#v1").click(() => $("#video").attr('src', "../assets/Videos/v1.mp4"));
+$("#v2").click(() => $("#video").attr('src', "../assets/Videos/v1.mp4"));
+$("#v3").click(() => $("#video").attr('src', "../assets/Videos/v1.mp4"));
+$("#v4").click(() => $("#video").attr('src', "../assets/Videos/v1.mp4"));
+$("#v5").click(() => $("#video").attr('src', "../assets/Videos/v1.mp4"));
+$("#v6").click(() => $("#video").attr('src', "../assets/Videos/v1.mp4"));
 $("#playVideo").click(() => $("#video").trigger('play'));
 $("#pauseVideo").click(() => $("#video").trigger('pause'));
