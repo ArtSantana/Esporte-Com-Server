13dbfc0213fb705381aa2af654e43c09308be839
artsantana <artsantana1457@hotmail.com>
2020-02-19 14:03:14 -0300

    Refatoração

--- src/client/viewOnTouch/index.js
+++ src/client/viewOnTouch/index.js
@@ -1,49 +1,45 @@
-let playSwitch = 0;
 let recFlag = null;
 let recording = false;
 let gravadorPost = 0
-
+const dbInformation = getData();
+let matrizes = new Array();
+let playSwitch = 0;
 // Removendo o clique direito da aplicação
 document.addEventListener('contextmenu', event => event.preventDefault());
-// Deixando os jogadores draggable
 for(i=1; i<12; i++){	
 	$("#b"+i).draggable();
 	$("#r"+i).draggable();    
 }
 
-const dbInformation = getData();
-let matrizes = new Array();
-
-Promise.resolve(dbInformation).then(dataReceived =>{
+Promise.resolve(dbInformation).then(dataReceived => {
 	matrizes = dataReceived;
 })
 
-$("#play").click(function(){
+$("#play").click(function() {
 	GoPosition(playSwitch);
 });
-
-$("#presetButton1").click(function(){
+$("#presetButton1").click(function() {
 	playSwitch = 0;
 	ReturnPosition(playSwitch);
 });
 
-$("#presetButton2").click(function(){
+$("#presetButton2").click(function() {
 	playSwitch = 1;
 	ReturnPosition(playSwitch);	
 });
-$("#presetButton3").click(()=>{
+$("#presetButton3").click(()=> {
 	playSwitch = 2;
 	ReturnPosition(playSwitch);
 })
-$("#presetButton4").click(()=>{
+$("#presetButton4").click(()=> {
 	playSwitch = 3;
 	ReturnPosition(playSwitch);
 })
-$("#presetButton5").click(()=>{
+$("#presetButton5").click(()=> {
 	playSwitch = 4;
 	ReturnPosition(playSwitch);
 })
-$("#presetButton6").click(()=>{
+$("#presetButton6").click(()=> {
 	playSwitch = 5;
 	ReturnPosition(playSwitch);
 })
